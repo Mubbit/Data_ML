0398d3699ba861a142e5b907b99f9600306bebf7
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2022-07-15 19:52:04 +0900

    220715 api connection done

--- preprocess.py
+++ preprocess.py
@@ -16,9 +16,9 @@ class ParseLemmJson():
                 term=[]
                 uris=[]
                 for item in json_file.items(): 
-                    #내부에는 Term, 그리고 uri 묶음이 있다
+                    #tuple인 item 내부에는 Term, 그리고 uri 묶음이 있다
                     term.append(item[0].split('/')[-1])
-                    print("The term of json object:",term[-1]) #Term과 나머지(uri 덩이)
+                    print("The term of json object:",term[-1], item[-1].keys()) #Term과 나머지(uri 덩이)
                     uris.append(item[-1])#tuple of relevant uri 
                 return term, uris #[단어], [(관련uri튜플)]
             except json.decoder.JSONDecodeError:
@@ -26,5 +26,38 @@ class ParseLemmJson():
                      
         print("failed to open file")
         return -1
+    def toPandas(self):
+        term,uris=self.open()
+        return pd.DataFrame(term,columns=['vocab'])
+
+#test panrye
+'''
+dependency: bs4
+'''
+import socket
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+import requests
+class ParsePanrye:
+    #check IP, set ID, connect
+    def __init__(self) -> None:
+        hostname=socket.gethostname()
+        ipaddr=socket.gethostbyname(hostname)
+        print('''
+              class is initialized, 
+              be sure your ip is authenticated by the api
+              your ip: {}'''.format(ipaddr))
+    def connect(self):
+        self.myId=input('\ngive us your id').strip('\n')
+        self.apilink='https://www.law.go.kr/DRF/lawSearch.do?OC={self.myId}&target=prec&type=XML'
+        response = urlopen(self.apilink).read()
+        print(bs(response))
+    def connect2(self):
+        self.myId=input('\ngive us your id').strip('\n')
+        self.apilink='https://www.law.go.kr/DRF/lawSearch.do?OC={}&target=prec&type=XML'.format(self.myId)
+        response=requests.get(self.apilink)
+        soup = bs(response.text, 'xml')
+        return soup
         
-    
+    def test_tokenizePerP(tx):
+        pass
